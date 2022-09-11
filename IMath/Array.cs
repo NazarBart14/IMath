4633da10fa6abe397043ecb63a959ab21a7f6468
Lenovo <Lenovo@DESKTOP-AJ5INH1>
2022-09-11 13:23:26 +0300

    Добавьте файлы проекта.

---
+++ IMath/Array.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMath
+{
+    internal class Array : IMath
+    {
+        int[] sered = new int[] {123, 214, 84, 47, 165, 213, 23, 243, 234, 275 };
+
+       
+       
+        public int Max()
+        {
+            Console.WriteLine(sered.Max());
+            return sered.Max();
+        }
+        public int Min()
+        {
+            Console.WriteLine(sered.Min());
+            return sered.Min();
+        }
+        public float Avg()
+        {
+            Console.WriteLine(sered.Average());
+            return 0;
+        }
+        public bool Search(int @value)
+        {
+            bool flag = false;
+            foreach (int i in sered)
+            {
+                if(i == @value)
+                {
+                    flag = true;
+                }
+            }
+            return flag;
+           
+        }
+    }
+}
---
+++ IMath/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMath
+{
+    public interface IMath
+    {
+        int Max();
+        int Min();
+        float Avg();
+        bool Search(int @value);
+    }
+   
+
+    internal class Program : Array
+    {
+        static void Main(string[] args)
+        {
+            
+            Array array = new Array();
+            array.Max();
+            array.Min();
+            array.Avg();
+            Console.WriteLine(array.Search(1000));
+        }
+    }
+}
